abad90a67c3a66abd1aa7d254f46d1239f17a67a
Christian Fortes <christianf@pandorga.com.br>
2016-05-18 18:16:03 -0300

    Change files

--- public/development/gruntfile.js
+++ public/development/gruntfile.js
@@ -1,65 +1,67 @@
-module.exports = function (grunt) {
-	var config = require('./config.json');
-	var path = config.directory;
+module.exports = function(grunt) {
+    var config = require('./config.json');
+    var path = config.directory;
+    var cssFileName;
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		watch: {
-			config: {
-				files: ['gruntfile.js', 'package.json'],
-				options: {
-					livereload: true
-				}
-			},
-			css: {
-				files: [path.sass + '*.scss', path.sass + '**/*.scss'],
-				tasks: ['sass:dev'],
-				options: {
-					spawn: true,
-					livereload: true
-				},
-			}
-		},
-		sass: {
-			dev: {
-				options: {
-					noCache: true,
-					lineNumbers: true,
-					update: true,
-					style: 'expanded',
-					sourcemap: 'none'
-				},
-				files: {
-					[path.css + "main.css"]: [path.sass + "main.scss"],
-					[path.css + "home.css"]: [path.sass + "pages/_home.scss"],
-				}
-			},
-			prod: {
-				options: {
-					noCache: true,
-					lineNumbers: false,
-					update: false,
-					style: 'compressed',
-					sourcemap: 'none'
-				},
-				files: [{
-					expand: true,
-					cwd: path.sass,
-					src: ['*.scss'],
-					dest: path.css,
-					ext: '.css'
-				}]
-			}
-		}
-	});
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            config: {
+                files: ['gruntfile.js', 'package.json'],
+                options: {
+                    livereload: true
+                }
+            },
+            css: {
+                files: [path.sass + '*.scss', path.sass + '**/*.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: true,
+                    livereload: true
+                },
+            }
+        },
+        sass: {
+            dev: {
+                options: {
+                    noCache: true,
+                    lineNumbers: true,
+                    update: true,
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: [path.css + cssFileName]
+            },
+            prod: {
+                options: {
+                    noCache: true,
+                    lineNumbers: false,
+                    update: false,
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: path.sass,
+                    src: ['*.scss'],
+                    dest: path.css,
+                    ext: '.css'
+                }]
+            }
+        }
+    });
 
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.event.on('watch', function(action, filepath, target, outro) {
+        cssFileName = filepath + '.tscss';
+    });
 
-	// Default task(s).
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['sass:prod']);
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass:prod']);
 
 };
